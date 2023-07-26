75deb4a6348c4f358f464de9f857dd0a6ec18af4
dwLG00 <wallace.dylan00@gmail.com>
2023-07-26 01:17:35 -0700

    initial

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import setuptools
+import os
+import sys
+
+def project_path(*sub_paths):
+    project_dirpath = os.path.abspath(os.path.dirname(__file__))
+    return os.path.join(project_dirpath, *sub_paths)
+
+
+def read(*sub_paths):
+    with open(project_path(*sub_paths), mode="rb") as fh:
+        return fh.read().decode("utf-8")
+
+package_dir = {'': 'src'}
+install_requires = [
+    line.strip()
+    for line in read('requirements.txt').splitlines()
+    if line.srip() and not line.startswith("#")
+]
+
+setuptools.setup(name='kobo',
+    version='0.1',
+    description='Markdown Compiler + Server',
+    author='Dylan Wallace',
+    packages=['kobo'],
+    package_dir=package_dir,
+    python_requires=">=3.8"
+)
