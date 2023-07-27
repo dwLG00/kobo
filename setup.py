7eb97f9d44556b5902cbca927c67d73da3f6775e
dwLG00 <wallace.dylan00@gmail.com>
2023-07-26 21:18:03 -0700

    Added requirements directly in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-VERSION = '0.1.1'
+VERSION = '0.1.2'
 
 import setuptools
 import os
@@ -8,21 +8,7 @@ import sys
 import pathlib
 
 
-def project_path(*sub_paths):
-    project_dirpath = os.path.abspath(os.path.dirname(__file__))
-    return os.path.join(project_dirpath, *sub_paths)
-
-
-def read(*sub_paths):
-    with open(project_path(*sub_paths), mode="rb") as fh:
-        return fh.read().decode("utf-8")
-
 package_dir = {'': 'src'}
-install_requires = [
-    line.strip()
-    for line in read('requirements.txt').splitlines()
-    if line.strip() and not line.startswith("#")
-]
 
 PACKAGE_DIR = pathlib.Path(__file__).parent
 README = (PACKAGE_DIR / 'README.md').read_text()
@@ -43,5 +29,11 @@ setuptools.setup(
         'resources/static/images',
         'resources/content/*'
     ]},
-    python_requires=">=3.8"
+    python_requires=">=3.8",
+    install_requires=[
+        'flask',
+        'bs4',
+        'markdown',
+        'markdown-katex'
+    ]
 )
